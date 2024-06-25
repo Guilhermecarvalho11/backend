635dd11bbb720fb855afb41fecb79fb9a7f91d0d
Guilherme Carvalho <carvalho_guilhermegomes@hotmail.com>
2024-06-25 18:26:53 -0300

    checking if the user already exists

--- src/controllers/UserControllers.js
+++ src/controllers/UserControllers.js
@@ -7,17 +7,24 @@
      */
 
 const AppError = require('../ultils/AppError');
+const sqliteConection = require('../database/sqlite');
+const { response } = require('express');
 
 // O controller é responsavel em lidar com o processamento das informações
 class UserControllers { 
 
-    create(req, res) {
-        const {name, email, password, team} = req.body;
+    async create(req, res) {
+     const {name, email, password, team} = req.body;
 
-        if(!name){
-            throw new AppError('Nome é obrigatorio')
-        }
-        res.status(201).json({name, email, password, team})
+     const database = await sqliteConection();
+     const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+      if(checkUserExists){
+        throw new AppError("este email ja esta cadastrado")
+      }
+      
+      return response.status(201).json();
+
     }
 
 }
