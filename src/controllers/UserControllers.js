dd30ab0de66970b2a79b91211825f2e7071c5c73
Guilherme Carvalho <carvalho_guilhermegomes@hotmail.com>
2024-06-25 18:36:07 -0300

    register user

--- src/controllers/UserControllers.js
+++ src/controllers/UserControllers.js
@@ -20,9 +20,11 @@ class UserControllers {
      const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
 
       if(checkUserExists){
-        throw new AppError("este email ja esta cadastrado")
+        throw new AppError("este email ja está cadastrado")
       }
-      
+
+      await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, password])
+
       return response.status(201).json();
 
     }
